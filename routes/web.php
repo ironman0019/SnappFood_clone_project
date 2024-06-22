3d45aed20dd25aabee63dc1ff3d0f5a8f01de1a1
ironman0019 <emadesavi@gmail.com>
2024-06-23 02:12:08 +0330

    Creating admin panel routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/admin/login', [AdminController::class, 'login']);
+
+Route::get('/admin', [AdminController::class, 'index']);
+
+Route::post('/admin/auth', [AdminController::class, 'auth']);
