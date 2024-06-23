3f5103fa0d33eb0faa840af806d2d92ea28e38ae
ironman0019 <emadesavi@gmail.com>
2024-06-24 02:31:04 +0330

    Adding logout & update password route for admin

--- routes/web.php
+++ routes/web.php
@@ -7,8 +7,21 @@ use Illuminate\Support\Facades\Route;
 //     return view('welcome');
 // });
 
+// Show admin login form
 Route::get('/admin/login', [AdminController::class, 'login']);
 
+//  Show admin dashbord page
 Route::get('/admin', [AdminController::class, 'index']);
 
+// authtenticate admin
 Route::post('/admin/auth', [AdminController::class, 'auth']);
+
+// Log out admin
+Route::post('/admin/logout', [AdminController::class, 'logout']);
+
+// Show admin edit username & password form
+Route::get('/admin/change_password', [AdminController::class, 'edit']);
+
+// Update admin username & password
+Route::put('/admin/update', [AdminController::class, 'update']);
+
