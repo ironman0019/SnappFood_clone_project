01c0022cf490b958a1f1878ab1d1314e018cd97a
ironman0019 <emadesavi@gmail.com>
2024-06-28 01:43:54 +0330

    Create admin resturents tags & food tags CRUD routes

--- routes/web.php
+++ routes/web.php
@@ -25,3 +25,21 @@ Route::get('/admin/change_password', [AdminController::class, 'edit']);
 // Update admin username & password
 Route::put('/admin/update', [AdminController::class, 'update']);
 
+// Show admin resturents tags page
+Route::get('/admin/resturents_tags', [AdminController::class, 'resturentsTagsIndex']);
+
+// Store admin resturents tag in database
+Route::post('/admin/resturentTagStore', [AdminController::class, 'resturentsTagsStore']);
+
+// Delete admin resturents tag
+Route::delete('/admin/resturents_tags/{resturentTag}', [AdminController::class, 'deleteResTag']);
+
+// Show admin food tags page
+Route::get('/admin/food_tags', [AdminController::class, 'foodTagsIndex']);
+
+// Store admin food tag in database
+Route::post('/admin/foodTagStore', [AdminController::class, 'foodTagsStore']);
+
+// Delete admin food tag
+Route::delete('/admin/food_tags/{foodTag}', [AdminController::class, 'deleteFoodTag']);
+
