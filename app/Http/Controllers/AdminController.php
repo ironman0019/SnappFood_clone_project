5507eb351750861907f051af0e56b2c7f565421c
ironman0019 <emadesavi@gmail.com>
2024-06-28 01:54:47 +0330

    Adding admin resturent & food tags CRUD functions

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Models\Admin;
+use App\Models\FoodTag;
+use App\Models\ResturentTag;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -98,6 +100,7 @@ class AdminController extends Controller
         return back()->withErrors(['username' => 'invalid credentials'])->onlyInput('username');
     }
 
+    // Logout admin
     public function logout(Request $request)
     {
         Auth::guard('admin')->logout();
@@ -107,4 +110,60 @@ class AdminController extends Controller
 
         return redirect('/admin/login')->with('message', 'You have been logged out!');
     }
+
+    // Show resturents tag page view
+    public function resturentsTagsIndex()
+    {
+        return view('admin.resturents_tags', ['tags' => ResturentTag::all()]);
+    }
+
+    // Create resturent tag and store in database
+    public function resturentsTagsStore(Request $request, ResturentTag $resturentTag)
+    {
+        $formFields = $request->validate([
+            'tag' => 'required',
+        ]);
+
+        $resturentTag->create($formFields);
+
+        return back()->with('message', 'Tag created successfully!');
+    }
+
+    // Delete resturents tag
+    public function deleteResTag(ResturentTag $resturentTag)
+    {
+        // make sure user is auth!   
+
+        $resturentTag->delete();
+        return redirect('/admin/resturents_tags')->with('delMessage', 'tag deleted!');
+
+    }
+
+    // Show food tag page view
+    public function foodTagsIndex()
+    {
+        return view('admin.food_tags', ['tags' => FoodTag::all()]);
+    }
+
+    // Create food tag and store in database
+    public function foodTagsStore(Request $request, FoodTag $foodTag)
+    {
+        $formFields = $request->validate([
+            'tag' => 'required',
+        ]);
+
+        $foodTag->create($formFields);
+
+        return back()->with('message', 'Tag created successfully!');
+    }
+
+    // Delete food tag
+    public function deleteFoodTag(FoodTag $foodTag)
+    {
+        // make sure user is auth!   
+
+        $foodTag->delete();
+        return redirect('/admin/food_tags')->with('delMessage', 'tag deleted!');
+    }
+
 }
