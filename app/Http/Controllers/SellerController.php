6531307ba7f44672c8ede7b95f628a5991cbbcc5
ironman0019 <emadesavi@gmail.com>
2024-06-30 00:51:39 +0330

    Create seller controller & add functions for register

---
+++ app/Http/Controllers/SellerController.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Resturent;
+use App\Models\Seller;
+use App\Rules\mobileNo;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+
+class SellerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('sellers.register');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $formFields = $request->validate([
+            'name' => ['required', 'min:3'],
+            'email' => ['required', 'email', Rule::unique('sellers', 'email')],
+            'phone' => ['required', new mobileNo],
+            'password' => 'required|confirmed|min:4',
+        ]);
+
+        // Hash password
+        $formFields['password'] = bcrypt($formFields['password']);
+
+        // Create seller account
+        $seller = Seller::create($formFields);
+
+        // Login seller
+        auth()->guard('seller')->login($seller);
+        $seller_id = auth()->guard('seller')->id();
+
+        // Create resturent for this new seller
+        Resturent::create([
+            'seller_id' => $seller_id,
+            'name' => '0',
+            'tag' => '0',
+            'phone' => '0',
+            'address' => '0',
+            'bank' => '0',
+        ]);
+
+        // redirect 
+        return redirect('/seller/dashbord')->with('message', 'You are logged in!');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+
+    // Show resturent complete info form
+    public function createResturentInfo()
+    {
+        return view('sellers.resturents_info');
+    }
+}
