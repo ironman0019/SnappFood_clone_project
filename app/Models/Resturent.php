6664106fa6d2b71465a268805eec2893b05804fa
ironman0019 <emadesavi@gmail.com>
2024-07-03 20:46:38 +0330

    Add relation with sellers & foods table

--- app/Models/Resturent.php
+++ app/Models/Resturent.php
@@ -10,4 +10,16 @@ class Resturent extends Model
     use HasFactory;
 
     protected $fillable = ['seller_id', 'name', 'tag', 'phone', 'address', 'bank', 'photo', 'delivary_price', 'work_hours', 'status'];
+
+    // Relation with seller
+    public function seller()
+    {
+        return $this->belongsTo(Seller::class, 'seller_id');
+    }
+
+    // Relation with foods
+    public function foods()
+    {
+        return $this->hasMany(Food::class, 'resturent_id');
+    }
 }
