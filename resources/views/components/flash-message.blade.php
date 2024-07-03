5eb1fbb51d4665897c4e6cd7eb19947b00254eb8
ironman0019 <emadesavi@gmail.com>
2024-07-03 20:49:08 +0330

    Create another session for delete flash messages

--- resources/views/components/flash-message.blade.php
+++ resources/views/components/flash-message.blade.php
@@ -3,4 +3,11 @@
 <div x-data="{show: true}" x-init="setTimeout(() => show= false,3000)" x-show="show" class="alert alert-success" role="alert">
     {{session('message')}}
 </div>
- @endif
+@endif
+
+<!-- Delete flash message -->
+@if(session()->has('delMessage'))
+<div x-data="{show: true}" x-init="setTimeout(() => show= false,3000)" x-show="show" class="alert alert-danger" role="alert">
+    {{session('delMessage')}}
+</div>
+@endif
