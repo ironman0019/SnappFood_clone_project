b364798f20d7254b734b8a307e98a8d2cb631c31
ironman0019 <emadesavi@gmail.com>
2024-07-18 01:27:15 +0330

    Add flash message to seller login page

--- resources/views/sellers/login.blade.php
+++ resources/views/sellers/login.blade.php
@@ -9,7 +9,9 @@
   <!-- Bootstrap cdn -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   <!-- icon cdn -->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer"/>
+    <!-- alpine js cdn -->
+    <script src="//unpkg.com/alpinejs" defer></script>
   <title>Document</title>
 </head>
 
@@ -18,6 +20,12 @@
 
   <!-- Show case -->
   <section class="tw-bg-pink-300 tw-h-screen">
+    <!-- flash message -->
+    @if(session()->has('message'))
+    <div x-data="{show: true}" x-init="setTimeout(() => show= false,3000)" x-show="show" class="alert alert-success" role="alert">
+      {{session('message')}}
+    </div>
+    @endif
     <div class="d-flex">
       <div class="m-3">
         <div class="tw-ml-8 tw-mt-5">
