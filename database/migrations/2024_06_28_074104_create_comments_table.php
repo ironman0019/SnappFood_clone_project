7b8a1241c381cd6dea5e850797a39e9bfb9771a2
ironman0019 <emadesavi@gmail.com>
2024-07-18 01:15:35 +0330

    Add references for user_id & order_id in comments migration

--- database/migrations/2024_06_28_074104_create_comments_table.php
+++ database/migrations/2024_06_28_074104_create_comments_table.php
@@ -13,8 +13,8 @@ return new class extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
-            $table->integer('user_id');
-            $table->integer('order_id');
+            $table->foreignId('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreignId('order_id')->references('id')->on('orders')->onDelete('cascade');
             $table->text('body');
             $table->text('reply')->nullable();
             $table->boolean('delete_req')->default(false);
