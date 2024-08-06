29e280c091cb20c26da00c5ee6721fc53fc4d9ae
ironman0019 <emadesavi@gmail.com>
2024-08-07 01:24:51 +0330

    Add input type of file to admin resturent_tags view for uploading picture for resturent_tags

--- resources/views/admin/resturents_tags.blade.php
+++ resources/views/admin/resturents_tags.blade.php
@@ -1,22 +1,21 @@
 <x-admin_layout>
 
-    <!-- flash message -->
-    @if(session()->has('delMessage'))
-    <div x-data="{show: true}" x-init="setTimeout(() => show= false,3000)" x-show="show" class="alert alert-danger" role="alert">
-        {{session('delMessage')}}
-    </div>
-    @endif
-
-
     <!-- Add resturents tags form -->
     <section class="container m-5 bg-secondary-subtle py-5 shadow-lg">
-        <form action="/admin/resturentTagStore" method="POST" class="w-50 mx-auto">
+        <form action="/admin/resturentTagStore" method="POST" enctype="multipart/form-data" class="w-50 mx-auto">
             @csrf
             <h4>Adding resturents tags</h4>
             <div class="mb-3 mt-3">
                 <label for="" class="form-label">Tag</label>
                 <input type="text" class="form-control" id="" name="tag">
                 @error('tag')
+                <div class="form-text text-danger">{{$message}}</div>
+                @enderror
+            </div>
+            <div class="mb-3">
+                <label for="" class="form-label">Picture</label>
+                <input type="file" class="form-control" id="" name="picture">
+                @error('picture')
                 <div class="form-text text-danger">{{$message}}</div>
                 @enderror
             </div>
@@ -31,6 +30,7 @@
                 <tr>
                     <th scope="col">#</th>
                     <th scope="col">Tag</th>
+                    <th scope="col">pic</th>
                     <th scope="col">Delete</th>
                 </tr>
             </thead>
@@ -45,6 +45,13 @@
                 <tr>
                     <th scope="row">{{$i}}</th>
                     <td>{{$tag->tag}}</td>
+                    <td>
+                        @if($tag->picture)
+                        <img src="{{asset('storage/' . $tag->picture)}}" alt="" class="w-25 h-25">
+                        @else
+                        {{'Null'}}
+                        @endif
+                    </td>
                     <td>
                         <form action="resturents_tags/{{$tag->id}}" method="POST">
                             @csrf
