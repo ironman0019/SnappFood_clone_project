ff1d2453d4f3a82a483db4f92c3846af4edb90c8
ironman0019 <emadesavi@gmail.com>
2024-08-24 01:13:55 +0330

    Create cart model & add relation with food & user tables

---
+++ app/Models/Cart.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Cart extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['food_id', 'resturent_id', 'quantity'];
+
+    // Relation with food
+    public function food()
+    {
+        return $this->belongsTo(Food::class, 'food_id');
+    }
+
+    // Relation with user
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+}
