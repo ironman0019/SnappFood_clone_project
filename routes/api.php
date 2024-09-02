093325eae07933192330ec6f9fe4ff569dc2c373
ironman0019 <emadesavi@gmail.com>
2024-09-02 17:31:19 +0330

    Add api routes for rateings

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Api\ApiAdminController;
 use App\Http\Controllers\Api\ApiCartController;
 use App\Http\Controllers\Api\ApiCommentController;
 use App\Http\Controllers\Api\ApiOrderController;
+use App\Http\Controllers\Api\ApiRateController;
 use App\Http\Controllers\Api\ApiUserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -31,5 +32,7 @@ Route::group(['middleware' => 'auth:sanctum'], function() {
     Route::apiResource('orders', ApiOrderController::class);
     Route::apiResource('carts', ApiCartController::class);
     Route::apiResource('comments', ApiCommentController::class);
+    Route::post('rateings', [ApiRateController::class, 'store']);
+    Route::put('rateings/{rate}', [ApiRateController::class, 'update']);
     Route::post('/logout', [ApiUserController::class, 'logout']);
 });
