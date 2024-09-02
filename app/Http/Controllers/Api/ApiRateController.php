2579203eff20cc5394b20eb641e4c5bb664519f7
ironman0019 <emadesavi@gmail.com>
2024-09-02 17:30:49 +0330

    Create api rate controller

---
+++ app/Http/Controllers/Api/ApiRateController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Rate;
+use Illuminate\Http\Request;
+
+class ApiRateController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $userId = auth()->user()->id;
+        $resturent_id = $request->input('resturent_id');
+        $rate = $request->input('rate');
+
+        // Create rate for resturent
+        Rate::create([
+            'user_id' => $userId,
+            'resturent_id' => $resturent_id,
+            'rate' => $rate
+        ]);
+
+        return response([
+            'message' => 'comment created!'
+        ],201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Rate $Rate)
+    {
+        $resturent_id = $request->input('resturent_id');
+        $rate = $request->input('rate');
+
+        // Update rateing
+        $Rate->update([
+            'resturent_id' => $resturent_id,
+            'rate' => $rate
+        ]);
+
+        return response([
+            'message' => 'Rateing Updated!'
+        ],200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
