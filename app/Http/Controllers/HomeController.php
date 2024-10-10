1f585e7d8f3c9b9df4f4af954a659ee3410416b9
ironman0019 <emadesavi@gmail.com>
2024-10-11 00:45:12 +0330

    Add functions for ordering food

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Cart;
 use App\Models\Food;
+use App\Models\Order;
+use App\Models\OrderFoodItem;
 use App\Models\Resturent;
 use App\Models\ResturentTag;
 use GuzzleHttp\Client;
@@ -39,6 +42,59 @@ class HomeController extends Controller
             'foods' => $foods,
             'newResturents' => $newResturents,
         ]);
+    }
+
+    public function foodOrder($id)
+    {
+        
+        $foods = Food::where('resturent_id', $id)->get();
+        return view('food_order', [
+            'foods' => $foods,
+            'resturentId' => $id
+        ]);
+    }
+
+    public function addToCart(Request $request)
+    {
+
+        // Get the order data from the AJAX request
+        $orderData = $request->input('order');
+        $resturentId = $request->input('resturentId');
+        $totalPrice = $request->input('totalPrice');
+
+        if (!$orderData || count($orderData) === 0) {
+            return response()->json(['success' => false, 'message' => 'No items in order']);
+        }
+
+        // Find the resturent
+        $resturent = Resturent::find($resturentId);
+
+        // Create order
+        Order::create([
+            'user_id' => auth()->user()->id,
+            'resturent_id' => $resturentId,
+            'address' => auth()->user()->address,
+            'delivary_fee' => $resturent->delivary_price,
+            'total_amount' => $totalPrice
+        ]);
+
+        // Find the order just created
+        $order = Order::where('user_id', auth()->user()->id)->where('created_at', now())->first();
+
+        foreach ($orderData as $item) {
+            // You can access $item['id'], $item['name'], $item['price'], and $item['quantity']
+            // For example:
+            // Order::create(['food_id' => $item['id'], 'quantity' => $item['quantity'], ...]);
+            // Crate order_food_item
+            OrderFoodItem::create([
+                'order_id' => $order->id,
+                'food_id' => $item['id'],
+                'quantity_ordered' => $item['quantity']
+            ]);
+        }
+
+        // Respond with success
+        return response()->json(['success' => true, 'message' => 'Order Submitted!']);
     }
 
     /**
