842b799c36e653f4cd9675f98ac5c24e7c6dd0fb
ironman0019 <emadesavi@gmail.com>
2024-10-20 16:08:01 +0330

    Add section for create comment & rateing

--- resources/views/order_detaile.blade.php
+++ resources/views/order_detaile.blade.php
@@ -1,5 +1,8 @@
 <x-home_layout>
 
+    <!-- flash message -->
+    <x-flash-message />
+
     <section class="tw-h-screen tw-bg-pink-600 tw-pt-5">
         <div class="container">
             <table class="table table-striped">
@@ -12,6 +15,7 @@
                         <th scope="col">status</th>
                         <th scope="col">Total_amount</th>
                         <th scope="col">Resturent</th>
+                        <th scope="col">Action</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -31,6 +35,12 @@
                         <td>{{$order->order_status}}</td>
                         <td class="fw-bold">{{number_format($order->total_amount)}} Toman</td>
                         <td>{{$order->resturent->name}}</td>
+                        <td>
+                            <div class="d-flex gap-2">
+                                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#comment-modal">Leave a comment</button>
+                                <button type="button" class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#rate-modal">Rate this resturent</button>
+                            </div>
+                        </td>
                     </tr>
                 </tbody>
             </table>
@@ -43,6 +53,71 @@
             </a>
         </div>
     </section>
+
+
+    <!-- Comment Modal -->
+    <div class="modal fade" id="comment-modal">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="comment-modal">Create comment</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <form action="/create_comment/{{$order->id}}" method="POST" id="create-comment">
+                        @csrf
+                        <div class="mb-3">
+                            <label for="" class="form-label">Comment</label>
+                            <input type="text" class="form-control" id="" name="body">
+                            @error('body')
+                            <div class="form-text text-danger">{{$message}}</div>
+                            @enderror
+                        </div>
+                    </form>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="submit" form="create-comment" class="btn btn-primary px-5">Submit<i class="fa-solid fa-check mx-2"></i></button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <!-- Rate Modal -->
+    <div class="modal fade" id="rate-modal">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="rate-modal">Rate the resturent</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <form action="/rate_resturent/{{$order->resturent_id}}" method="POST" id="create-rate">
+                        @csrf
+                        <div class="mb-3">
+                            <label for="" class="form-label">Rate</label>
+                            <select class="form-select" name="rate">
+                                <option selected>Rate the resturent</option>
+                                <option value="1">One</option>
+                                <option value="2">Two</option>
+                                <option value="3">Three</option>
+                                <option value="4">Four</option>
+                                <option value="5">Five</option>
+                            </select>
+                            @error('rate')
+                            <div class="form-text text-danger">{{$message}}</div>
+                            @enderror
+                        </div>
+                    </form>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="submit" form="create-rate" class="btn btn-primary px-5">Submit<i class="fa-solid fa-pen-to-square mx-2"></i></button>
+                </div>
+            </div>
+        </div>
+    </div>
 
 
 
