149fecd8fdeeea83d265f4f786f5bb853720f272
Emad Isavi <emadesavi@gmail.com>
2024-11-10 01:41:35 +0330

    Create order-status update notification

---
+++ app/Notifications/OrderStatusUpdate.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class OrderStatusUpdate extends Notification implements ShouldQueue
+{
+    use Queueable;
+
+    public $order;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct($order)
+    {
+        $this->order = $order;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        return (new MailMessage)
+                    ->line('Your order status with order_id:' . $this->order->id . 'has been updated!')
+                    ->action('See order_detaile', url('http://localhost:8000/order_detaile/' . $this->order->id))
+                    ->line('order created_at:' . $this->order->created_at . ' order status:' . $this->order->order_status);
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            //
+        ];
+    }
+}
